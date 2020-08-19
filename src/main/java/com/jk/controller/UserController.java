15b316d5a748ab557eb78146522dfa115003b736
wjl20010805 <528922443@qq.com>
2020-08-19 14:49:29 +0800

    添加hello方法

--- src/main/java/com/jk/controller/UserController.java
+++ src/main/java/com/jk/controller/UserController.java
@@ -1,9 +1,16 @@
 package com.jk.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * @author : wjl
  * @date : 14:34 2020/8/19
  * @user : lenovo
  */
 public class UserController {
+
+    @RequestMapping("/hello")
+    public void hello(){
+
+    }
 }
