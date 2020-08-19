8a4ac2274c2678315b5f74e083d33f0888f311f8
wjl20010805 <528922443@qq.com>
2020-08-19 15:01:39 +0800

    添加Controller注解

--- src/main/java/com/jk/controller/UserController.java
+++ src/main/java/com/jk/controller/UserController.java
@@ -1,9 +1,13 @@
 package com.jk.controller;
 
+import org.springframework.stereotype.Controller;
+
 /**
  * @author : wjl
  * @date : 14:34 2020/8/19
  * @user : lenovo
  */
+@Controller
 public class UserController {
+
 }
