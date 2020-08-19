8c55d5063ad6828ddc740faafd44392a8c6dc34c
wjl20010805 <528922443@qq.com>
2020-08-19 15:14:02 +0800

    打个招呼

--- src/main/java/com/jk/controller/UserController.java
+++ src/main/java/com/jk/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.jk.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * @author : wjl
@@ -10,4 +11,8 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class UserController {
 
+    @RequestMapping("/hi")
+    public String hi(){
+        return "hi zhaoshun";
+    }
 }
